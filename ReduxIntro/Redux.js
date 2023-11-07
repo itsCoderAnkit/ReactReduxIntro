ad8bf47dabdf9561ea2442e2b92e615bda3e3616
ANKIT <itsankitagarwal000@gmail.com>
2023-11-07 11:44:23 +0530

    first redux code- inc. and dec.

---
+++ ReduxIntro/Redux.js
@@ -0,0 +1,36 @@
+const redux = require('redux')
+
+const counterReducer = (state={counter:0},action)=>{
+    if(action.type==='increment'){
+        return{
+            counter:state.counter+1
+        }
+    }
+    if(action.type ==='decrement'){
+        return{
+            counter:state.counter-1
+        }
+    }
+    if(action.type==='incrementby2'){
+        return{
+            counter:state.counter+2
+        }
+    }
+    if(action.type==='decrementby2'){
+        return{
+            counter:state.counter-2
+        }
+    }
+}
+
+const store = redux.createStore(counterReducer)
+
+const counterSubscriber = ()=>{
+    const latestState = store.getState()
+    console.log(latestState)
+}
+
+store.subscribe(counterSubscriber)
+
+store.dispatch({type:'incrementby2'})
+store.dispatch({type:'decrementby2'})
